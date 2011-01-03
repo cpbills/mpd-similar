0145fd791f83bc832a44799baf567df5d966b9d5
Christopher P. Bills <cpbills@fauxtographer.net>
2011-01-02 23:29:50 -0500

    update

--- mpd-similar.pl
+++ mpd-similar.pl
@@ -1,5 +1,13 @@
 #!/usr/bin/perl
 
+# script to generate a playlist from a given track using audioscrobber to find
+# similar tracks and in the event that doesn't work, similar artists...
+# in theory it will find a few tracks for the given track, and then find tracks
+# for the tracks found, somewhat recursively.
+
+# track.getsimilar&artist=artist&track=track&limit=50&api_key=key
+# artist.getsimilar&artist=artist&limit=20&api_key=key
+
 use strict;
 use warnings;
 
@@ -10,6 +18,14 @@ my $API_KEY     = 'api key goes here';
 my $TRACK_URL   = 'http://ws.audioscrobbler.com/2.0/?method=track.getsimilar';
 my $ARTIST_URL  = 'http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar';
 
+my $MPD_HOST    = 'localhost';
+my $MPD_PORT    = '6600';
+my $MPD_PASS    = '';
 
+my $mpd = Audio::MPD->new(host      =>  $MPD_HOST,
+                          port      =>  $MPD_PORT,
+                          password  =>  $MPD_PASS);
 
-&artist=cher&track=believe&api_key=b25b959554ed76058ac220b7b2e0a026&limit=100
+my $status = $mpd->status;
+
+print $status;
