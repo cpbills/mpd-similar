a0c033b920284a2151da8b0febb73f64eb532d2c
Christopher P. Bills <cpbills@fauxtographer.net>
2011-01-09 10:56:08 -0500

    mpd-similar.pl - fixed the logic for similar artists lookup, IF the script ever makes it that far. currently, it is disabled code...

--- mpd-similar.pl
+++ mpd-similar.pl
@@ -5,8 +5,8 @@
 # in theory it will find a few tracks for the given track, and then find tracks
 # for the tracks found, somewhat recursively.
 
-# track.getsimilar&artist=artist&track=track&limit=50&api_key=key
-# artist.getsimilar&artist=artist&limit=20&api_key=key
+# track.getsimilar&artist=artist&track=track&api_key=key
+# artist.getsimilar&artist=artist&api_key=key
 
 use strict;
 no strict "refs";
@@ -21,16 +21,19 @@ use DBI;
 my $SIZE        = 10;   # number of tracks to try to add, by default
    $SIZE        = $ARGV[0] if ($ARGV[0] && $ARGV[0] =~ /[0-9]+/);
 my $CROP        = 0;    # leave the current song and remove everything else
+my $VERBOSE     = 0;
 my $CLEAR       = 0;    # completely clear the playlist
 my $AUTOPLAY    = 1;    # attempt to resume / start play after adding a song
 my $SHUFFLE     = 0;    # shuffle playlist after each song is added.
+my $SEED        = 5;    # number of songs to add based on the initial song
+my $PERSONG     = 3;    # number of songs to add based on seeded songs.
+my $M_MID       = 40;   # middle threshold for matching songs (percent)
+my $M_LOW       = 20;   # low threshold for matching songs (percent)
 
 my $KEY         = 'api key goes here';
 my $TRACK_URL   = 'http://ws.audioscrobbler.com/2.0/?method=track.getsimilar';
 my $ARTIST_URL  = 'http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar';
 my $ARTIST_ALT  = 'http://ws.audioscrobbler.com/2.0/artist/ARTIST/similar.txt';
-my $T_LIMIT     = 200;  # number of similar tracks to limit our search to
-my $A_LIMIT     = 20;   # number of similar artists to limit our search to
 my $DELIMITER   = ''; # ctrl-_ is a useful delimiter
 my $MAX_AGE     = 4*24*60*60; # 4 days should be a good start to check this out
 
@@ -66,6 +69,20 @@ unless ($artist) {
     print "no artist information provided or available from mpd playlist";
     exit 1;
 }
+
+# call a function that will check that all files referenced
+# in the database exist on disk and are up to day. TODO
+#purge_db() if $PURGE;
+
+
+
+
+# SEEDING -
+# either seed based off the track provided, give title, artist and file
+# or; given a title and artist;
+#   if that track is available, add it
+#   else use it's info only to seed.
+# or; given only an artist, use a random song by that artist to seed
 
 # TODO:
 # new algorithm;
@@ -106,23 +123,55 @@ while (scalar(keys %PLAYLIST) < $SIZE) {
     if ($song) {
         $MPD->playlist->add($song);
         $song = $MPD->collection->song($song);
-        print "Added " . $$song{artist} . ' - ' . $$song{title} . "\n";
+        print "$artist - $title -> $$song{artist} - $$song{title}\n"
+            if ($VERBOSE);
+        #print "Added " . $$song{artist} . ' - ' . $$song{title} . "\n";
         $MPD->playlist->shuffle if ($SHUFFLE);
         $MPD->play unless ($MPD->status->state eq 'play' or !$AUTOPLAY);
     }
 
     my @playlist = $MPD->playlist->as_items;
     if (scalar(@playlist) == 0) {
-        print "unable to find any songs to match from\n";
+        print "playlist empty; nothing was found with provided information\n";
         exit;
     }
-    fisher_yates_shuffle(\@playlist);
+    fisher_yates_shuffle(\@playlist) if (scalar(@playlist) > 0);
     my $song_to_match = pop(@playlist);
     $artist = $$song_to_match{artist};
     $title  = $$song_to_match{title};
     $file   = $$song_to_match{file};
 }
+
+sub check_db {
+    my $file    = shift;
+
+    my %matches = ();
+    my %used    = ();
+    my $dbh = db_connect();
+    my $now = time;
+    $file = $dbh->quote($file);
+
+    my $select = qq{ select song_id,last_update from songs where file=$file };
+    my ($song_id,$last) = $dbh->selectrow_array($select);
+
+    # return our empty hash if the query was unsuccessful
+    return %matches unless ($song_id);
+    # return our empty hash if the entry needs to be refreshed
+    return %matches if ($last and $last > $MAX_AGE);
+
+    $select = qq{ select file,percent from matches where song_id=$song_id };
+    my $results = $dbh->selectall_hashref($select,'file');
     
+    my $sql = qq{ select last_used from songs where file = ? };
+    my $sth = $dbh->prepare($select);
+    foreach my $file (keys %$results) {
+        $used{$file} = 0;
+        $sth->execute($file);
+        my ($last) = $sth->fetchrow_array;
+        $used{$file} = $last if ($last);
+    }
+}
+
 sub get_similar {
     my $artist  = shift;
     my $title   = shift;
@@ -130,6 +179,9 @@ sub get_similar {
 
     my @songs   = ();
     my $updated = 0;
+
+    #my %songs2 = check_db($file) if ($file);
+
     if ($file) {
         # we're trying to do this the 'right' way, which is a
         # pain in the ass, and a lot of extra code.
@@ -144,22 +196,28 @@ sub get_similar {
             if ($last and $similar and (($now - $last) < $MAX_AGE));
         $dbh->disconnect;
     }
+
     #@songs = @{$SIMILAR{$file}} if ($file and $SIMILAR{$file});
     unless (@songs and scalar(@songs) > 0) {
         # if we have an api key and the track name, we can look for
         # similar tracks, and not just similar artists...
-        @songs = get_similar_tracks($artist,$title,$T_LIMIT,$KEY)
+        @songs = get_similar_tracks($artist,$title,$KEY)
             if ($KEY and $title);
 
         # we only want to store the results to our database if we got
         # a song / artist hit... otherwise it could dillute the pool
-        $updated = 1 if (@songs and scalar(@songs) > 0);
+        $updated = 1 if (scalar(@songs) > 0);
+
+        if (scalar(@songs) == 0) {
+            print "no similar tracks found for $artist - $title\n";
+        } 
 
         # if @songs is still void, we'll search by artist only
         # takes $KEY as a param, but is not required to work
-        @songs = get_similar_artists($artist,$A_LIMIT,$KEY)
-            unless (@songs and scalar(@songs) > 0);
+        #@songs = get_similar_artists($artist,$KEY)
+        #    unless (@songs and scalar(@songs) > 0);
     }
+
     # store our results for this song so we don't have to search
     # in the future, generally a good thing for speeding things up
     if ($file and $updated) {
@@ -183,16 +241,13 @@ sub get_similar {
 
     #@{$SIMILAR{$file}} = @songs if ($file);
 
-    # shuffle the array of found songs like a deck of cards
-    fisher_yates_shuffle(\@songs);
+    fisher_yates_shuffle(\@songs) if (scalar(@songs) > 0);
 
-    if (@songs) {
-        while (scalar(@songs) > 0) {
-            my $new_song = pop(@songs);
-            if ($new_song and !$PLAYLIST{$new_song}) {
-                $PLAYLIST{$new_song} = 1;
-                return $new_song;
-            }
+    while (scalar(@songs) > 0) {
+        my $new_song = pop(@songs);
+        if ($new_song and !$PLAYLIST{$new_song}) {
+            $PLAYLIST{$new_song} = 1;
+            return $new_song;
         }
     }
     return 0;
@@ -201,22 +256,24 @@ sub get_similar {
 sub get_similar_tracks {
     my $artist  =   shift;
     my $track   =   shift;
-    my $limit   =   shift;
     my $key     =   shift;
 
+    my @similar = ();
     my $sartist = uri_escape($artist);
     my $strack  = uri_escape($track);
-    my $params = "&artist=$sartist&track=$strack&limit=$limit&api_key=$key";
-
+    my $params  = "&artist=$sartist&track=$strack&api_key=$key";
     my $content = get("$TRACK_URL$params");
+
     unless ($content) {
-        print "failed to get: $TRACK_URL$params\n";
-        return;
+        # return an empty array when the web breaks, and we're unable
+        # to look up the artist/track. we could potentially put in a 
+        # retry limit... so TODO retry limit ?
+        print STDERR "failed to get: $TRACK_URL$params\n";
+        return @similar;
     }
     my $xml = new XML::Simple;
     my $results = $xml->XMLin($content);
 
-    my @similar = ();
     if ($$results{status} eq 'ok') {
         foreach my $track (@{$$results{similartracks}{track}}) {
             if (ref($track) eq 'HASH') {
@@ -230,7 +287,7 @@ sub get_similar_tracks {
     # track... i.e. the track title is something like 'Blah (Accoustic)'
     if ((scalar(@similar) == 0) and ($track =~ /\s*\(.*\)$/)) {
         $track =~ s/\s*\(.*\)$//;
-        return get_similar_tracks($artist,$track,$limit,$key);
+        return get_similar_tracks($artist,$track,$key);
     }
 
     my @finds = ();
@@ -250,41 +307,40 @@ sub get_similar_tracks {
 
 sub get_similar_artists {
     my $artist  =   shift;
-    my $limit   =   shift;
     my $key     =   shift;
     my $sartist = uri_escape($artist);
 
     my @similar = ( $artist );
 
     if ($key and $key ne '') {
-        my $params = "&artist=$sartist&limit=$limit&api_key=$key";
+        my $params = "&artist=$sartist&api_key=$key";
         my $content = get("$ARTIST_URL$params");
-        unless ($content) {
-            print "failed to get: $ARTIST_URL$params\n";
-            return undef;
-        }
-        my $xml = new XML::Simple;
-        my $results = $xml->XMLin($content);
-        if ($$results{status} eq 'ok') {
-            foreach my $s_artist (@{$$results{similarartists}{artist}}) {
-                push(@similar,$$s_artist{name}) if (ref $s_artist eq 'HASH');
+        if ($content) {
+            my $xml = new XML::Simple;
+            my $results = $xml->XMLin($content);
+            if ($$results{status} eq 'ok') {
+                my $s_artists = $$results{similarartists}{artist};
+                foreach my $s_artist (@$s_artists) {
+                  push(@similar,$$s_artist{name}) if (ref $s_artist eq 'HASH');
+                }
             }
+        } else {
+            print STDERR "failed to get: $ARTIST_URL$params\n";
         }
     } else {
         my $url = $ARTIST_ALT;
         $url =~ s/ARTIST/$sartist/;
         my $content = get($url);
-        unless ($content) {
-            print "failed to get: $url\n";
-            return undef;
+        if ($content) {
+            # map basically generates an array by applying the 'split' to each
+            # line of $content, it's a 'slick' way to do a foreach loop, without
+            # the loop. this is mostly a note for myself, because i don't use
+            # map too often, and i wanted to be clever...
+            @similar = map((split(/,/,$_))[2],split(/\n/,$content));
+        } else {
+            print STDERR "failed to get: $url\n";
         }
-        # map basically generates an array by applying the 'split' to each
-        # line of $content, it's a 'slick' way to do a foreach loop, without
-        # the loop. this is mostly a note for myself, because i don't use
-        # map too often, and i wanted to be clever...
-        @similar = map((split(/,/,$_))[2],split(/\n/,$content));
     }
-
     my @songs = ();
     foreach my $s_artist (@similar) {
         push(@songs,$MPD->collection->songs_by_artist($s_artist));
